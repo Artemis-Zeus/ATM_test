0b885ee117d3b27d2f45cb1796d2e9fa755c7d2b
Artemis <77585444+Believe-or-never@users.noreply.github.com>
2022-12-01 00:17:18 +0800

    readme.md已提交新的数据库格式 test.py可以输出当前的数据库所有内容

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import sqlite3
 
+
 def dict_factory(cursor, row):
     d = {}
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
     return d
-list1=["account","record","investment"]
-con = sqlite3.connect(".db")
+
+
+list1 = ["account", "record", "investment"]
+con = sqlite3.connect("ATM.db")
 # 三个数据库/表，替换上面db文件可以查看数据库里面的格式
 # atm_databse/atm
 # deposit/record
@@ -14,7 +17,9 @@ con = sqlite3.connect(".db")
 
 con.row_factory = dict_factory
 c = con.cursor()
-c.execute("select * from record ")
-output = c.fetchall()
-for i in output:
+for i in list1:
     print(i)
+    c.execute("select * from %s " % i)
+    output = c.fetchall()
+    for i in output:
+        print(i)
