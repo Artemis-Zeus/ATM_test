f5fa52e9baebf11b04ff01e3ca25bbe6ada7ce3a
Artemis <77585444+Believe-or-never@users.noreply.github.com>
2022-11-30 19:24:09 +0800

    db稍微修改

--- create.py
+++ create.py
@@ -7,12 +7,25 @@ import time
 import random
 
 # 在内存中创建数据库
-con = sqlite3.connect("Investment.db")
+db_name="ATM.db"
+con = sqlite3.connect(db_name)
 
 # 创建游标对象
 cur = con.cursor()
 
 # 新建表abc，包含id，name，age三列，sqlite可以省略类型,ID为主键（主键不能重复）并且不能为空，若已有该表则报错
+cur.execute("CREATE TABLE investment(id INT, t INT, rate FLOAT, money INT)")
+cur.execute("INSERT INTO investment(id,t,rate,money) VALUES(?,?,?,?)", (1, 1, 1.2, 1000))
+cur.execute("INSERT INTO investment(id,t,rate,money) VALUES(?,?,?,?)", (2, 2, 1.4, 1200))
+cur.execute("INSERT INTO investment(id,t,rate,money) VALUES(?,?,?,?)", (10, 3, 1.6, 2500))
+
+cur.execute("CREATE TABLE investment(id INT, t INT, rate FLOAT, money INT)")
+cur.execute("INSERT INTO investment(id,t,rate,money) VALUES(?,?,?,?)", (1, 1, 1.2, 1000))
+time.sleep(2)
+cur.execute("INSERT INTO investment(id,t,rate,money) VALUES(?,?,?,?)", (2, 2, 1.4, 1200))
+time.sleep(2)
+cur.execute("INSERT INTO investment(id,t,rate,money) VALUES(?,?,?,?)", (10, 3, 1.6, 2500))
+
 cur.execute("CREATE TABLE investment(id INT, t INT, rate FLOAT, money INT)")
 cur.execute("INSERT INTO investment(id,t,rate,money) VALUES(?,?,?,?)", (1, 1, 1.2, 1000))
 cur.execute("INSERT INTO investment(id,t,rate,money) VALUES(?,?,?,?)", (2, 2, 1.4, 1200))
